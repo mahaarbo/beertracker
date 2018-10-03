ca5b9517ae53658c2651c4533bff232bb304c121
Mathias Hauan Arbo <mathias.arbo@ntnu.no>
2018-10-03 20:51:33 +0200

    Initial commit

---
+++ beertracker.ino
@@ -0,0 +1,13 @@
+// WiFi Initialize
+#include <ESP8266WiFi.h> //for board specific
+#include <WiFiClient.h> //Arduino library for wificlients
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
